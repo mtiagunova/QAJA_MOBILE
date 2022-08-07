86f42a2fc9d791f37b38f1eff4a0db8bbd55f371
Mariia Tiagunova <mariiatiagunova@gmail.com>
2022-08-07 21:01:38 +0300

    test_2 added to project

--- src/test/java/tests/SearchTest.java
+++ src/test/java/tests/SearchTest.java
@@ -1,7 +1,6 @@
 package tests;
 
 import lib.CoreTestCase;
-//import lib.ui.MainPageObject;
 import lib.ui.MainPageObject;
 import org.junit.Test;
 import org.openqa.selenium.WebElement;
@@ -10,7 +9,6 @@ public class SearchTest extends CoreTestCase {
 
     @Test
     public void testFindJava() {
-
         MainPageObject mainPO = new MainPageObject(this.driver);
         WebElement searchInit = mainPO.waitForElementPresent(
                 "id:org.wikipedia:id/search_container",
@@ -28,5 +26,26 @@ public class SearchTest extends CoreTestCase {
         expectedResult.click();
 
         System.out.println("Success");
+    }
+
+    @Test
+    public void testFindNonExistentText() {
+        MainPageObject mainPO = new MainPageObject(this.driver);
+        WebElement searchInit = mainPO.waitForElementPresent(
+                "id:org.wikipedia:id/search_container",
+                "Cannot find Search Wikipedia init search input", 10);
+        searchInit.click();
+
+        WebElement searchInput = mainPO.waitForElementPresent(
+                "id:org.wikipedia:id/search_src_text",
+                "Cannot find Search... input");
+        searchInput.sendKeys("wfewfewfwegweg");
+
+        WebElement expectedResult = mainPO.waitForElementPresent(
+                "id:org.wikipedia:id/search_empty_text",
+                "Cannot find element 'No results found'");
+        if(expectedResult.getText().equals("No results found")) {
+            System.out.println("Success!");
+        }
     }
 }
